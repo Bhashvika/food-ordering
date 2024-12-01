9f3e60d8f46cd9d9d082c0565c25fe2a5361d0c3
Bosi Bhashvika <108601714+Bhashvika@users.noreply.github.com>
2024-12-01 21:00:43 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 export const connectDB=async ()=>{
-     await mongoose.connect('mongodb+srv://bhashvika:9618487809@cluster0.992fxxi.mongodb.net/Food-Delivery')
+     await mongoose.connect('mongodb+srv://bhashvika:9618487809@cluster0.992fxxi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
      .then(()=>console.log("database connected"))
 }
